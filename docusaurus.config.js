7ed65972ed4da3d42cf5aff46a3b0196c7bba09c
boon21 <bboonsir@uwaterloo.ca>
2024-01-02 21:18:53 -0500

    github pages settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/boonboonsiri.github.io/`,
+  baseUrl: `/`,
 
   plugins: ['docusaurus-plugin-sass'],
 
