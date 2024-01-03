fe71c61b99fcd1df27d185914a84042c7b3d1fb7
boon21 <bboonsir@uwaterloo.ca>
2024-01-02 20:00:07 -0500

    about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,35 @@ function HomepageHeader() {
   );
 }
 
+function AboutSection() {
+
+  return (
+    <div className="about-wrapper">
+      <div className="about-section">
+        <img className="profile" src='img/precroppedsmall.png' alt="Your Image Alt Text" />
+
+        <div className="text-wrapper">
+          <div className="title">
+            About
+          </div>
+
+          <div className="body">
+            Hi! I'm Boon, a software engineering student at the University of Waterloo graduating in 2024. I love coding, maps, reading, and may try to relearn how to play some instruments eventually. Check out my photography learning curve and tech blog (to do sometime).
+          </div>
+
+          <div className="links">
+
+            resume | <a href="https://github.com/boonboonsiri" target="_blank">github</a> | linkedin
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -27,6 +56,7 @@ export default function Home() {
       title={'Boon Boonsiri'}
       description="My personal site">
       <HomepageHeader />
+      <AboutSection />
     </Layout>
   );
 }
